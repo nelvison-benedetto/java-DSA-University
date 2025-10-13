c7e7189b49d091d080ad3ac7bc3c094f9aa2dfa3
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-10-13 10:31:23 +0200

    divideetconquer

--- course1_AlgorithmicToolbox/module4_DivideEtConquer/BinarySearchWithDuplicates.java
+++ course1_AlgorithmicToolbox/module4_DivideEtConquer/BinarySearchWithDuplicates.java
@@ -5,39 +5,68 @@ public class BinarySearchWithDuplicates {
         //in input una sequenza di numeri(anche cloni) anche integer q.
         //trova l'index dello slot nella sequenza dove si trova il PRIMO q esistente, return -1 se non esiste.s
 
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] K = new int[n];
-        for (int i = 0; i < n; i++) {
-            K[i] = input.nextInt();
-        }
-        int m = input.nextInt();
-        int[] Q = new int[m];
-        for (int i = 0; i < m; i++) {
-            Q[i] = input.nextInt();
-        }
-        for (int i = 0; i < m; i++) {
-            System.out.print(firstOccurrence(K, Q[i]));
-            if (i < m - 1) System.out.print(" ");
-        }
-        input.close();
+        // Scanner input = new Scanner(System.in);  //solution naive by AI
+        // int n = input.nextInt();
+        // int[] K = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     K[i] = input.nextInt();
+        // }
+        // int m = input.nextInt();
+        // int[] Q = new int[m];
+        // for (int i = 0; i < m; i++) {
+        //     Q[i] = input.nextInt();
+        // }
+        // for (int i = 0; i < m; i++) {
+        //     System.out.print(firstOccurrence(K, Q[i]));
+        //     if (i < m - 1) System.out.print(" ");
+        // }
+        // input.close();
     }
-    private static int firstOccurrence(int[] arr, int key) {
-        int left = 0, right = arr.length - 1;
-        int result = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (arr[mid] == key) {
-                result = mid;   //trovata un'occorrenza
-                right = mid - 1;  //cerchiamo più a sinistra
-            } else if (arr[mid] < key) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    // private static int firstOccurrence(int[] arr, int key) {  //solution naive by AI
+    //     int left = 0, right = arr.length - 1;
+    //     int result = -1;
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
+    //         if (arr[mid] == key) {
+    //             result = mid;   //trovata un'occorrenza
+    //             right = mid - 1;  //cerchiamo più a sinistra
+    //         } else if (arr[mid] < key) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid - 1;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+
+    //PERFECT SOLUTION 95% by me!!
+    public static int[] findFirstOccurrences( int[] numsToFind, int[] sortedArr ){
+         //sortedArr è order ascendente, se target num ha piu cloni in sortedArr return solo idx primo
+         int[] res = new int[numsToFind.length];
+        for(int i=0; i<res.length; i++){
+            res[i] = binary_search_returnIdxfirstclone(numsToFind[i], sortedArr);
         }
-        return result;
+        return res;
+    }
+    public static int binary_search_returnIdxfirstclone(int target, int[] arr){  //O(log n)
+        int left = 0;
+        int right = arr.length-1;
+        int res = -1;
+        boolean isFound = false;
+        while(left<=right && !isFound){
+            int mid = (left+right)/2;
+            if(arr[mid] == target){
+                // while(arr[mid-1]!=target){  !!error xk magari ci sono 1000 clones non puoi scorrere, meglio continuare ad usare divideetimper
+                //     mid--;
+                // }
+                // return mid;
+                res = mid;
+                right= mid-1; //🔥continua ad usare divideetimper
+            }
+            else if(arr[mid]<target) left=mid+1;
+            else right=mid-1;
+        }
+        return res;
     }
 }
--- course1_AlgorithmicToolbox/module4_DivideEtConquer/MajorityElement.java
+++ course1_AlgorithmicToolbox/module4_DivideEtConquer/MajorityElement.java
@@ -6,40 +6,48 @@ public class MajorityElement {
         //Input: A sequence of n integers.
         //Output: 1, if there is an element that is repeated more than n/2 times, and 0 otherwise. 
         //strategy: utilizzo di  Boyer–Moore Majority Vote Algorithm
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = input.nextInt();
-        }
-        int candidate = findCandidate(arr);
-        if (isMajority(arr, candidate)) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        } 
-        input.close();
+
+        // Scanner input = new Scanner(System.in);  //solution naive by AI
+        // int n = input.nextInt();
+        // int[] arr = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     arr[i] = input.nextInt();
+        // }
+        // int candidate = findCandidate(arr);
+        // if (isMajority(arr, candidate)) {
+        //     System.out.println(1);
+        // } else {
+        //     System.out.println(0);
+        // } 
+        // input.close();
+    }
+    // private static int findCandidate(int[] arr) {  //solution naive by AI
+    //     int count = 0, candidate = -1;
+    //     for (int num : arr) {
+    //         if (count == 0) {
+    //             candidate = num;
+    //             count = 1;
+    //         } else if (num == candidate) {
+    //             count++;
+    //         } else {
+    //             count--;
+    //         }
+    //     }
+    //     return candidate;
+    // }
+    // private static boolean isMajority(int[] arr, int candidate) {  //solution naive by AI
+    //     int count = 0;
+    //     for (int num : arr) {
+    //         if (num == candidate) count++;
+    //     }
+    //     return count > arr.length / 2;
+    // }
+
+    public static int getMajorityElement(int[] sequence){
+
+        
 
     }
-    private static int findCandidate(int[] arr) {
-        int count = 0, candidate = -1;
-        for (int num : arr) {
-            if (count == 0) {
-                candidate = num;
-                count = 1;
-            } else if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        return candidate;
-    }
-    private static boolean isMajority(int[] arr, int candidate) {
-        int count = 0;
-        for (int num : arr) {
-            if (num == candidate) count++;
-        }
-        return count > arr.length / 2;
-    }
+
+
 }
