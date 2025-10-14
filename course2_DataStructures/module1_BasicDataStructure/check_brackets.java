e2001c0ad69a3bc2ed0eeca497389399e0af2c85
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-10-14 16:52:26 +0200

    module1

--- course2_DataStructures/module1_BasicDataStructure/check_brackets.java
+++ course2_DataStructures/module1_BasicDataStructure/check_brackets.java
@@ -58,7 +58,8 @@ public class check_brackets{
     public static void main(String[] args){}
     public static String checkBrackets(String text){
 
-        
+        int x=1000;
+        return "bst";
 
     }
 }
