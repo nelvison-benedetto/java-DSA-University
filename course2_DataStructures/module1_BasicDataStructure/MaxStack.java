268a759426f63e06f506cb0d9a0964c542fc2e32
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-10-21 11:36:22 +0200

    maxstack ok

--- course2_DataStructures/module1_BasicDataStructure/MaxStack.java
+++ course2_DataStructures/module1_BasicDataStructure/MaxStack.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class StackWithMax {
+public class MaxStack {
 
     // class FastScanner {
     //     StringTokenizer tok = new StringTokenizer("");
@@ -18,7 +18,6 @@ public class StackWithMax {
     //         return Integer.parseInt(next());
     //     }
     // }
-
     // public void solve() throws IOException {
     //     FastScanner scanner = new FastScanner();
     //     int queries = scanner.nextInt();
@@ -46,9 +45,48 @@ public class StackWithMax {
     //         }
     //     }
     // }
-    public static void main(String[] args) throws IOException {
-        new StackWithMax().solve();
-    }
 
+    //PERFECT SOLUTION!!! tutte 3 le opr sono O(1)
+    private Deque<Integer> stack = new ArrayDeque<>();
+    private Deque<Integer> maxStack = new ArrayDeque<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner( System.in );
+        int q = scanner.nextInt();
+        MaxStack maxStack = new MaxStack();
+        for( int i=0; i<q; i++ ){
+            String command = scanner.next();  //get input che puo voler che si esegua push/pop/max
+            switch( command ){
+                case "push":
+                    int value = scanner.nextInt();  //get input
+                    maxStack.push(value);
+                    break;
+                case "pop":
+                    maxStack.pop();
+                    break;
+                case "max":
+                    System.out.println( maxStack.max() );
+                    break;
+            }
+        }
+        scanner.close();
+    }
+    public void push( int value ){
+        stack.addFirst( value );
+        if( maxStack.isEmpty() ){
+            maxStack.addFirst( value );
+        } else {
+            maxStack.addFirst( Math.max(value, maxStack.peekFirst()) );  //find the max and add it, anche se rimane sempre stesso item duplicato, serve x track levels
+        }
+    }
+    public void pop(){
+        if( !stack.isEmpty() ){
+            stack.pollFirst();
+            maxStack.pollFirst();
+        }
+    }
+    public int max() {
+        return maxStack.peekFirst();
+    }
     
 }
